63ca5c1a8050d57da945c3c94a1a8b377bb7d6ae
adinayaub <adinajkarybekkyzy79@gmail.com>
2025-01-14 10:03:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
-
 function toggleSidebar(){
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('rotate')
-
   closeAllSubMenus()
 }
-
 function toggleSubMenu(button){
-
   if(!button.nextElementSibling.classList.contains('show')){
     closeAllSubMenus()
   }
-
   button.nextElementSibling.classList.toggle('show')
   button.classList.toggle('rotate')
 
@@ -22,7 +17,6 @@ function toggleSubMenu(button){
     toggleButton.classList.toggle('rotate')
   }
 }
-
 function closeAllSubMenus(){
   Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
     ul.classList.remove('show')
